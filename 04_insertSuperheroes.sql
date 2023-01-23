4c9f7444f51bb90924a7e1edbb95ce4e8809aa13
stefvtls <stefvtls@gmail.com>
2023-01-23 11:18:01 +0100

    FEAT: inserting values

---
+++ 04_insertSuperheroes.sql
@@ -0,0 +1,3 @@
+INSERT INTO superhero (name, alias, origin) VALUES ('Bruce Wayne', 'Batman',' Gotham City');
+INSERT INTO superhero (name, alias, origin) VALUES ('Geralt', 'Witcher', 'Rivia');
+INSERT INTO superhero (name, alias, origin) VALUES ('Queen Daenerys I Targaryen', 'Khaleesi', 'Dragonstone');
