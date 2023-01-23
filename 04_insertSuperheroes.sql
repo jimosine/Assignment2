c610644ed3052ba9a624543f692a6d5bb53b3777
Jim <buissinkjim@gmail.com>
2023-01-23 10:31:49 +0100

    Updates to 4th script
    Wasn't working correctly oops, now it does

--- 04_insertSuperheroes.sql
+++ 04_insertSuperheroes.sql
@@ -1,16 +1,16 @@
-INSERT INTO superhero(
+INSERT INTO superhero (name, alias, origin) VALUES (
 	'Clark Kent',
 	'Superman',
 	'DC'
 );
 
-INSERT INTO superhero(
+INSERT INTO superhero (name, alias, origin) VALUES (
 	'Peter Parker',
 	'Spiderman',
 	'Marvel'
 );
 
-INSERT INTO superhero(
+INSERT INTO superhero (name, alias, origin) VALUES (
 	'Bruce Wayne',
 	'Batman',
 	'DC'
