4f17995db3ccb47804873c5c215dfdebd6b2826e
Jim <buissinkjim@gmail.com>
2023-01-23 10:37:16 +0100

    FEAT: added 5th script
    Adding assistants

---
+++ 05_insertAssistants.sql
@@ -0,0 +1,14 @@
+INSERT INTO assistant (name, superhero_id) VALUES (
+	'Lois Lane',
+	1
+);
+
+INSERT INTO assistant (name, superhero_id) VALUES (
+	'Mary Jane',
+	2
+);
+
+INSERT INTO assistant (name, superhero_id) VALUES (
+	'Alfred Pennyworth',
+	3
+);
