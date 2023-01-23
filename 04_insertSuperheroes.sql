c3ca8b49e45bcc4762a379f1b095ef32b4af179f
Jim <buissinkjim@gmail.com>
2023-01-23 10:24:21 +0100

    FEAT: added 4th script
    Adding 3 superheroes

---
+++ 04_insertSuperheroes.sql
@@ -0,0 +1,17 @@
+INSERT INTO superhero(
+	'Clark Kent',
+	'Superman',
+	'DC'
+);
+
+INSERT INTO superhero(
+	'Peter Parker',
+	'Spiderman',
+	'Marvel'
+);
+
+INSERT INTO superhero(
+	'Bruce Wayne',
+	'Batman',
+	'DC'
+);
