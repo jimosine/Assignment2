570289535cc492889c1d5a81849c892e33a9be24
Jim <buissinkjim@gmail.com>
2023-01-23 10:46:57 +0100

    FEAT: added 6th script
    Added four powers, and linked the correct superheroes to their power(s)

---
+++ 06_powers.sql
@@ -0,0 +1,25 @@
+INSERT INTO power(name, description) VALUES (
+	'Flying',
+	'The ability to fly across the sky'
+);
+
+INSERT INTO power(name, description) VALUES (
+	'Web-shooting',
+	'The ability to shoot spiderwebs out of your hands'
+);
+
+INSERT INTO power(name, description) VALUES (
+	'Lasers',
+	'The ability to shoot lasers from your eyes'
+);
+
+INSERT INTO power(name, description) VALUES (
+	'Rich',
+	'The ability to buy whatever you want'
+);
+
+INSERT INTO superhero_power (superhero_id, power_id) VALUES (1, 1);
+INSERT INTO superhero_power (superhero_id, power_id) VALUES (1, 3);
+INSERT INTO superhero_power (superhero_id, power_id) VALUES (1, 4);
+INSERT INTO superhero_power (superhero_id, power_id) VALUES (2, 2);
+INSERT INTO superhero_power (superhero_id, power_id) VALUES (3, 4);
