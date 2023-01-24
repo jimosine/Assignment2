d5c7913c44110de65bf141564aad7611948b8aae
Jim <buissinkjim@gmail.com>
2023-01-24 13:23:57 +0100

    Started blank project
    Added the properties

---
+++ ChinookJDBC/src/main/java/no/accelerate/chinookjdbc/ChinookJdbcApplication.java
@@ -0,0 +1,13 @@
+package no.accelerate.chinookjdbc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChinookJdbcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChinookJdbcApplication.class, args);
+    }
+
+}
---
+++ ChinookJDBC/src/test/java/no/accelerate/chinookjdbc/ChinookJdbcApplicationTests.java
@@ -0,0 +1,13 @@
+package no.accelerate.chinookjdbc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChinookJdbcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
