cb10292a505011392ed26794e4a836f9af203ff1
stefvtls <stefvtls@gmail.com>
2023-01-24 13:42:09 +0100

    FEAT: added comment to main

--- ChinookJDBC/Chinook_JDBC/src/test/java/no/noroff/chinook_jdbc/ChinookJdbcApplicationTests.java
+++ ChinookJDBC/Chinook_JDBC/src/test/java/no/noroff/chinook_jdbc/ChinookJdbcApplicationTests.java
@@ -1,4 +1,4 @@
-package no.accelerate.chinookjdbc;
+package no.noroff.chinook_jdbc;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ChinookJdbcApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
