952efca17d7901c429fd68b193c34425e25810e0
stefvtls <stefvtls@gmail.com>
2023-01-25 12:19:33 +0100

    FEAT: added method to customer repository to find the most popular genre of customer with given ID. Updated CustomerGenre model

--- ChinookJDBC/Chinook_JDBC/src/main/java/no/noroff/chinook_jdbc/repository/CustomerRepository.java
+++ ChinookJDBC/Chinook_JDBC/src/main/java/no/noroff/chinook_jdbc/repository/CustomerRepository.java
@@ -1,6 +1,7 @@
 package no.noroff.chinook_jdbc.repository;
 
 import no.noroff.chinook_jdbc.models.Customer;
+import no.noroff.chinook_jdbc.models.CustomerGenre;
 
 import java.util.List;
 
@@ -8,5 +9,7 @@ public interface CustomerRepository extends CRUDRepository<Customer, Integer> {
     List<Customer> findSubset(int limit, int offset);
 
     List<Customer> findByName(String name);
+
+    List<CustomerGenre> findMostPopularGenreForCustomer(int customerId);
 }
 
